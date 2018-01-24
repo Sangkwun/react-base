4f9b75258348badd505d4fab6feeb1742f22a742
Sangkwun <wer2774@gmail.com>
2018-01-24 12:48:23 +0900

    finish to create Explain component

--- src/components/Welcome/presenter.js
+++ src/components/Welcome/presenter.js
@@ -25,9 +25,22 @@ const Welcome = (props, state) => (
 
 const Main = (props) => (
     <div className={styles.whiteBox}>
-        {props.loginPage? "loginPage":"Welcome"}
+        {props.loginPage? "loginPage":<Explain />}
     </div>
 )
+const Explain = () => (
+  <div className={styles.explain}>
+    <span className={styles.title}>결제를 인스테드하다.</span>
+
+    <div className={styles.splitLine} />
+
+    <span className={styles.description}>요청과 결제에서 생기는 불편함</span>
+    <span className={styles.description}>인스테드로 해결하세요.</span>
+    <a href="http://instead-corp.com/">
+      <button className={styles.button}>어떤 서비스인가요?</button>
+    </a>
+  </div>
+);
 
 
 Welcome.propTypes = {
